f203bce0b7e9b959df06d3e69f4756d389466515
hayden walker <haydenjwalker1@gmail.com>
2022-07-12 15:49:58 +1000

    Added my first component

---
+++ index.js
@@ -0,0 +1,13 @@
+function MainContent() {
+  return (
+    <h1>I'm learning React!</h1>
+  )
+}
+
+
+ReactDOM.render(
+  <div>
+    <MainContent />
+  </div>,
+  document.getElementById('root')
+  )
