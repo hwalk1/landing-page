75311402c0d97273529665ca3f453e24f003f16e
hwalk1 <haydenjwalker1@gmail.com>
2022-07-18 17:02:40 +1000

    Moved away from CDN to react npm install

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-// ** This shows the declarative nature of React, where we can tell it what to do
-// ** and it worries about the rest.
+import React from "react";
+import ReactDOM from "react-dom";
 
-// ** For equvilancy sake, className can be added
-ReactDOM.render(<h1 className="header">Hello, from React!</h1>, document.getElementById("root"))
+const navbar = (
+  <nav>
+    <h1>Hayden's Project Page</h1>
+    <ul>
+      <li>Projects</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+)
 
-
-
-
-
-// ** This is what the react DOM is creating when we are building it out.
-// test
-
-// const h1 = document.createElement("h1")
-// h1.textContent = "This is the way to program again"
-// h1.className = "header"
-// document.getElementById("root").append(h1)
+ReactDOM.render(navbar, document.getElementById("root"))
