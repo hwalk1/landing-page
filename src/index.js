2de36601e67ee1dabd7bc0cf001b5d692c3498ef
hwalk1 <haydenjwalker1@gmail.com>
2022-07-20 15:53:59 +1000

    Split out the original Page to Header, Body, Footer)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,46 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-function Page() {
-
+function Header() {
   return (
-    <div>
       <header>
         <nav>
           <img src="./logo192.png" alt="React Logo" width='40px' />
         </nav>
       </header>
+  );
+}
+
+function Footer() {
+  return (
+
+      <footer>
+        <p>2022 Walker Development. All rights reserverd</p>
+      </footer>
+  )
+}
+
+function Body() {
+  return (
+    <>
       <h1>Reasons I love React! </h1>
       <ol>
         <li>It's easy to use</li>
         <li>It's declarative</li>
         <li>It's a highly in demand skill</li>
       </ol>
-      <footer>
-        <p>2022 Walker Development. All rights reserverd</p>
-      </footer>
+    </>
+  )
+
+}
+
+function Page() {
+
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
