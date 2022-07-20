5b0c3de1d5b53c2eff1e68baf4de2bd3054f7903
hwalk1 <haydenjwalker1@gmail.com>
2022-07-20 17:12:26 +1000

    Created basic CSS styling of the page

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import ReactDOM from "react-dom/client"
 function Header() {
   return (
       <header>
-        <nav>
-          <img src="./logo192.png" alt="React Logo" width='40px' />
+        <nav className="nav">
+          <img src="./logo192.png" alt="React Logo" className="img" />
+          <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
         </nav>
       </header>
   );
@@ -14,7 +19,7 @@ function Header() {
 function Footer() {
   return (
 
-      <footer>
+      <footer className="footer">
         <p>2022 Walker Development. All rights reserverd</p>
       </footer>
   )
@@ -22,14 +27,14 @@ function Footer() {
 
 function Body() {
   return (
-    <>
+    <div className="content">
       <h1>Reasons I love React! </h1>
       <ol>
         <li>It's easy to use</li>
         <li>It's declarative</li>
         <li>It's a highly in demand skill</li>
       </ol>
-    </>
+    </div>
   )
 
 }
