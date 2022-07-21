85c321d904d39dcf6093ff825dc852cda60c2c2a
hwalk1 <haydenjwalker1@gmail.com>
2022-07-21 15:34:14 +1000

    Created seperate .js files for each landing page componenet

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Footer() {
+  return (
+
+      <footer className="footer">
+        <p>2022 Walker Development. All rights reserverd</p>
+      </footer>
+  )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -1,43 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import Header from "./Header"
+import Footer from "./Footer"
+import Body from "./Body"
 
-function Header() {
-  return (
-      <header>
-        <nav className="nav">
-          <img src="./logo192.png" alt="React Logo" className="img" />
-          <ul className="nav-items">
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-      </header>
-  );
-}
-
-function Footer() {
-  return (
-
-      <footer className="footer">
-        <p>2022 Walker Development. All rights reserverd</p>
-      </footer>
-  )
-}
-
-function Body() {
-  return (
-    <div className="content">
-      <h1>Reasons I love React! </h1>
-      <ol>
-        <li>It's easy to use</li>
-        <li>It's declarative</li>
-        <li>It's a highly in demand skill</li>
-      </ol>
-    </div>
-  )
-
-}
 
 function Page() {
 
