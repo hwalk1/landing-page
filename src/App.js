c80b3441b8e7d0d77b6110d63faafe44d4f7e079
hwalk1 <haydenjwalker1@gmail.com>
2022-07-22 16:24:02 +1000

    Initial set up of components & App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <div>
+      <Main />
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
